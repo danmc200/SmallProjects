a095ff141e972d9ee26689f5317ae017e6098fd2
Daniel Boydelatour <danielboydelatour@gmail.com>
2021-10-23 16:47:40 -0400

    add empty test for poker result calculator

---
+++ PokerHands/TestPokerHands.java
@@ -0,0 +1,16 @@
+package PokerHands;
+
+import org.junit.jupiter.api.Test;
+
+class TestPokerHands 
+{
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	private void fail(String string) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST FAILED: " + string);
+	}
+}
