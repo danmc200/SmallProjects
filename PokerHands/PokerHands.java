86504238121a22137d91ebc1ded2623c15d2cb45
Daniel Boydelatour <danielboydelatour@gmail.com>
2021-11-03 04:49:07 -0400

    Order Methods in PokerHands Class and adjust some of the access modifiers.

--- PokerHands/PokerHands.java
+++ PokerHands/PokerHands.java
@@ -34,71 +34,71 @@ public class PokerHands
 	private Map<String, List<String>> hands = new HashMap<String, List<String>>();
 	
 	
-	/*****Getters and Setters*****/
+	/**
+	 * A poker deck contains 52 cards - 
+	 * each card has a suit which is one of clubs, diamonds, hearts, or spades 
+	 * (denoted C, D, H, and S in the input data). 
+	 * Each card also has a value which is one of 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace 
+	 * (denoted 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A).
+	 * @param args -> "(Player Label: ) (cards)*5"
+	 */
+	public static void main(String [] args)
+    {
+		PokerHands pokerHands = new PokerHands();
+		pokerHands.scoreGameWithInput(pokerHands, args);
+    }
 	
 	/**
-	 * collects and returns the score results of each hand
-	 * @return the result of each hand dealt.
+	 * A poker deck contains 52 cards - 
+	 * each card has a suit which is one of clubs, diamonds, hearts, or spades 
+	 * (denoted C, D, H, and S in the input data). 
+	 * Each card also has a value which is one of 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace 
+	 * (denoted 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A).
+	 * @param args -> "(Player Label: ) (cards)*5"
 	 */
-	public Map<String, Map<Score, String[]>> collectHandResults()
+	public void scoreGameWithInput(PokerHands pokerHands, String [] args)
 	{
-		for(String s : hands.keySet())
+		int count = 1;
+		StringBuffer sb = new StringBuffer();
+		String 
+			hand="", 
+			player="";
+		
+		for(String s : args)
 		{
-			List<String> hnd = hands.get(s);
-			handResults.put(s, getScores(hnd));
-		}
-		return handResults;
-	}
-	
-	/**
-	 * @param key -> the key to match to gain result of hand dealt.
-	 * @return the result of hand dealt.
-	 */
-	public Map<Score, String []> getHandResult(String key)
-	{
-		for(String s : handResults.keySet())
-		{
-			if(s.equals(key))
+			sb.append(s);
+			if(count == (HAND_SIZE+1))//hand size plus label
 			{
-				return handResults.get(s);
+				sb.append(System.lineSeparator());
+				
+				player = sb.substring(0, sb.indexOf(":"));
+				hand = sb.substring(sb.indexOf(":") + 1);
+				
+				String [] handArr = inputToArray(hand);
+				
+				if(pokerHands.getHand(player) == null)
+				{
+					pokerHands.addHand(player, handArr);
+				}
+				else
+				{
+					pokerHands.printCardHands();
+					
+					pokerHands.compareScores();
+					
+					//add after game clears hands
+					pokerHands.addHand(player, handArr);
+				}
+				sb = new StringBuffer();
+				count = 0;
+				System.out.println();
 			}
+			count++;
 		}
-		return null;
+		//**run again b/c loop design
+		pokerHands.printCardHands();
+		pokerHands.compareScores();
 	}
-	
-	/**
-	 * @param playerLabel -> player label to match to give hand dealt.
-	 * @return hand dealt.
-	 */
-	public List<String> getHand(String playerLabel)
-	{
-		for(String s : hands.keySet())
-		{
-			if(s.equals(playerLabel))
-			{
-				return hands.get(s);
-			}
-		}
-		return null;
-	}
-	public Map<String, List<String>> getHands()
-	{
-		return hands;
-	}
-	
-	public void addHand(String playerLabel, List<String> hand)
-	{
-		hands.put(playerLabel, hand);
-	}
-	public void addHand(String playerLabel, String [] hand)
-	{
-		addHand(playerLabel, new ArrayList<String>(Arrays.asList(hand)));
-	}
-	public void clearHands()
-	{
-		hands = new HashMap<String, List<String>>();
-	}
-	
 	
 	/******SCORING*****/
 	
@@ -205,6 +205,71 @@ public class PokerHands
 			}
 		}
 		return methodAndReturn;
+	}
+	
+	/*****Getters and Setters*****/
+	
+	/**
+	 * collects and returns the score results of each hand
+	 * @return the result of each hand dealt.
+	 */
+	protected Map<String, Map<Score, String[]>> collectHandResults()
+	{
+		for(String s : hands.keySet())
+		{
+			List<String> hnd = hands.get(s);
+			handResults.put(s, getScores(hnd));
+		}
+		return handResults;
+	}
+	
+	/**
+	 * @param key -> the key to match to gain result of hand dealt.
+	 * @return the result of hand dealt.
+	 */
+	protected Map<Score, String []> getHandResult(String key)
+	{
+		for(String s : handResults.keySet())
+		{
+			if(s.equals(key))
+			{
+				return handResults.get(s);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * @param playerLabel -> player label to match to give hand dealt.
+	 * @return hand dealt.
+	 */
+	protected List<String> getHand(String playerLabel)
+	{
+		for(String s : hands.keySet())
+		{
+			if(s.equals(playerLabel))
+			{
+				return hands.get(s);
+			}
+		}
+		return null;
+	}
+	protected Map<String, List<String>> getHands()
+	{
+		return hands;
+	}
+	
+	protected void addHand(String playerLabel, List<String> hand)
+	{
+		hands.put(playerLabel, hand);
+	}
+	protected void addHand(String playerLabel, String [] hand)
+	{
+		addHand(playerLabel, new ArrayList<String>(Arrays.asList(hand)));
+	}
+	protected void clearHands()
+	{
+		hands = new HashMap<String, List<String>>();
 	}
 	
 	/*****HELPERS*******/
@@ -678,62 +743,4 @@ public class PokerHands
 		
 		return straight;
 	}
-	
-	public void scoreGameWithInput(PokerHands pokerHands, String [] args)
-	{
-		int count = 1;
-		StringBuffer sb = new StringBuffer();
-		String 
-			hand="", 
-			player="";
-		
-		for(String s : args)
-		{
-			sb.append(s);
-			if(count == (HAND_SIZE+1))//hand size plus label
-			{
-				sb.append(System.lineSeparator());
-				
-				player = sb.substring(0, sb.indexOf(":"));
-				hand = sb.substring(sb.indexOf(":") + 1);
-				
-				String [] handArr = inputToArray(hand);
-				
-				if(pokerHands.getHand(player) == null)
-				{
-					pokerHands.addHand(player, handArr);
-				}
-				else
-				{
-					pokerHands.printCardHands();
-					
-					pokerHands.compareScores();
-					
-					//add after game clears hands
-					pokerHands.addHand(player, handArr);
-				}
-				sb = new StringBuffer();
-				count = 0;
-				System.out.println();
-			}
-			count++;
-		}
-		//**run again b/c loop design
-		pokerHands.printCardHands();
-		pokerHands.compareScores();
-	}
-	
-	/**
-	 * A poker deck contains 52 cards - 
-	 * each card has a suit which is one of clubs, diamonds, hearts, or spades 
-	 * (denoted C, D, H, and S in the input data). 
-	 * Each card also has a value which is one of 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace 
-	 * (denoted 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A).
-	 * @param args -> "(Player Label: ) (cards)*5"
-	 */
-	public static void main(String [] args)
-    {
-		PokerHands pokerHands = new PokerHands();
-		pokerHands.scoreGameWithInput(pokerHands, args);
-    }
 }
